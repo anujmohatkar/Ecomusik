f62b931a22ee9df06225643ef4eab4aa5189cdaa
anujmohatkar <anujmohatkar1@gmail.com>
2022-01-04 13:04:31 +0530

    Added instrumnets table and configured carrierwave and some imp db migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :instruments
   devise_for :users, controllers: {
     regisrtrations: 'registrations'
   }
-  root to: 'store#index'
+  root to: 'instruments#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
